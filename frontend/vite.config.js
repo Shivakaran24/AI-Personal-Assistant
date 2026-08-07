92b8768d226dc56b789bbeb7cd104009aa566966
Shivakaran24 <dasarisunny124@gmail.com>
2026-08-07 22:04:29 +0530

    Updated Vitejs.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
-    port: 3001,
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://127.0.0.1:8000',
